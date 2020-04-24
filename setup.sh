0e01875b65f5bb75bbb8e2b511cfd543bf251c19
killbit <josh.smith.it@gmail.com>
2020-04-24 16:51:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 apt update
 apt install -y python3.8
 apt install -y python3-pip
+python3 -m pip install --upgrade pip setuptools
 pip3 install python-masscan
 pip3 install tabulate
 pip install ipaddress
