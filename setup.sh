5c94bc91a17e47cde7324210714f973a5f5cdc0d
killbit <josh.smith.it@gmail.com>
2020-04-24 18:19:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt install -y python3-pip
 python3 -m pip install --upgrade pip setuptools
 pip3 install python-masscan
 pip3 install tabulate
-pip install ipaddress
+pip3 install ipaddress
+pip3 install pandas
 sed -i 's/logger.debug/#&/' $(locate masscan.py)
 chmod 755 reachy.py
