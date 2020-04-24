5f5801d7695268d0c14beb0fb9754b3b8287c15c
killbit <josh.smith.it@gmail.com>
2020-04-24 18:14:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo '--------------THIS MUST BE RUN AS ROOT--------------'
 apt update
-apt install -y python3.8
+apt install -y python3
 apt install -y python3-pip
 python3 -m pip install --upgrade pip setuptools
 pip3 install python-masscan
