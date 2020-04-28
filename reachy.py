62d5bca0a778e17fbb1aee6edb7ac64db971200d
root <root@covpentest02.qpayprod.local>
2020-04-28 18:18:32 -0400

    Fixed Counting Method

--- reachy.py
+++ reachy.py
@@ -49,22 +49,27 @@ else:
     nm.scan(str(my_ranges[0]).strip("[]"), ports='7,9,13,21-23,25-26,37,53,79-81,88,106,110-111,113,119,135,139,143-144,179,199,389,427,443-445,465,513-515,543-544,548,554,587,631,646,873,990,993,995,1025-1029,1110,1433,1720,1723,1755,1900,2000-2001,2049,2121,2717,3000,3128,3306,3389,3986,4899,5000,5009,5051,5060,5101,5190,5357,5432,5631,5666,5800,5900,6000-6001,6646,7070,8000,8008-8009,8080-8081,8443,8888,9100,9999-10000,32768,49152-49157',arguments='--max-rate 10000')
 
 
+
+#initalize  counts of each value to zero in range_resultz dict
+for eachRange in my_ranges[1]:
+    range_resultz[eachRange] = 0
+
+#Create a dict of IP Address that responded with open ports
 for eachIP in nm.scan_result['scan'].keys():
-    for eachRange in my_ranges[1]:
-        if ipaddress.ip_address(eachIP) in eachRange.hosts():
-            if eachRange not in range_resultz.keys():
-                range_resultz[eachRange] = 1
-            else:
-                range_resultz[eachRange]=range_resultz[eachRange]+1
-        else:
-            range_resultz[eachRange] = 0
     for eachProto in nm.scan_result['scan'][eachIP]:
         portz=nm.scan_result['scan'][eachIP][eachProto].keys()
         scan_resultz[ipaddress.ip_address(eachIP)] = [eachProto, sorted(list(portz))]
+
+#Count IPs in Ranges
+for eachRange in my_ranges[1]:
+    for eachIP in scan_resultz.keys():
+        if eachIP in eachRange.hosts():
+            range_resultz[eachRange]=range_resultz[eachRange]+1
     
 sorted_resultz=OrderedDict(scan_resultz)
 df = pd.Series(sorted_resultz)
 
+# Convert all values in range results to strings for printing purposes
 for k,v in range_resultz.items():
     range_resultz[k]=str(v)
     
